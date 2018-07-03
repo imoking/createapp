e146794a0dc6f29302e77e9609537c28ec363fa5
EC2 Default User <ec2-user@ip-172-31-29-87.us-east-2.compute.internal>
2018-07-03 07:31:27 +0000

    第８章完

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,12 @@ class SessionsController < ApplicationController
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-    end
       render 'new'
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
